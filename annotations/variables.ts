a84b30eb3038956edf6f246abb9ec900794f8348
Jay Hernandez <jeric614@yahoo.com>
2021-09-22 18:40:47 +0900

    type annotations w/variables

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,1 +1,24 @@
-const apples: number = 5;
+//type annotation, if try diff type, will error
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+//built in objects
+let now: Date = new Date();
+
+//Array; going to have something of array that contains strings
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+//Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
