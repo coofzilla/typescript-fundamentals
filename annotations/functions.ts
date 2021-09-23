abd809c18ae362bbd7d98a8871be046c8fb71263
Jay Hernandez <jeric614@yahoo.com>
2021-09-23 22:35:16 +0900

    inference with functions, example why annotate return

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,3 +1,12 @@
+//annotate every argument & annotate what function will return
+//ts will analyze body of function
 const add = (a: number, b: number): number => {
   return a + b;
+};
+
+//make sure to annotate what function will return
+//if not situation like below can occur where ts thinks this is okay
+//no return statement in body doesn't allow for inference, ts doesn't flag error
+const subtract = (a: number, b: number) => {
+  a - b;
 };
