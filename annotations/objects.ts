5fa4ab006f160cf32bd1de627aa2336cbbab3a6b
coofzilla <jeric614@yahoo.com>
2022-02-05 21:23:59 +0900

    shorthand f/constructor w/private

--- annotations/objects.ts
+++ annotations/objects.ts
@@ -1,5 +1,5 @@
 const profile = {
-  name: 'alex',
+  name: "alex",
   age: 20,
   coords: {
     lat: 0,
@@ -10,8 +10,7 @@ const profile = {
   },
 };
 
-const { age }: { age: number } = profile;
-console.log(age);
+const { age, name }: { age: number; name: string } = profile;
 
 //es 2015
 // const {
@@ -22,3 +21,15 @@ const {
 }: { coords: { lat: number; lng: number } } = profile;
 
 // const { name: string } = profile;
+// const sam = {
+//   ...profile,
+//   name: "sam",
+//   age: 22,
+//   coords: { lat: 1, lng: 2 },
+//   setAge(age: number): void {
+//     this.age = age;
+//   },
+// };
+
+// console.log(sam);
+export {};
